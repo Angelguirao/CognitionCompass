1acc6e4018141c6bc0a20d9280b73f8d8f7d780e
Angel Guirao <angeguirao92@gmail.com>
2024-10-26 17:03:19 +0200

    Create Basic Express Server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
 const connectToDatabase = async () => {
   try {
@@ -12,3 +14,15 @@ const connectToDatabase = async () => {
 };
 
 connectToDatabase();
+
+// Middleware
+app.use(express.json());
+
+// Simple route
+app.get('/', (req, res) => {
+  res.send('Cognition Compass API');
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
